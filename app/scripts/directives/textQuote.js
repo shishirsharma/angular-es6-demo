d1ba026a08e49314dcd7ddc12a559d935300ec62
ramon <ramon@twintags.com>
2015-06-16 16:58:01 +0200

    Refactor directive

--- app/scripts/directives/textQuote.js
+++ app/scripts/directives/textQuote.js
@@ -1,22 +1,23 @@
 'use strict';
 'ngInject';
 
-class TextQuoteDirective { 
-  constructor() {
+const DIRECTIVE = new Map();
+
+class TextQuoteDirective {
+  constructor($log) {
     this.restrict = 'E';
     this.scope = {
       data: '='
     };
     this.template = '<blockquote>{{textInfo}}</blockquote>';
+
+    DIRECTIVE.set('$log', $log);
   }
 
   link (scope) {
     scope.textInfo = scope.data.toUpperCase();
-  }
-
-  static directiveFactory() {
-    return new TextQuoteDirective();
+    DIRECTIVE.get('$log').info(scope.textInfo);
   }
 }
 
-angular.module('angularEs6DemoApp').directive('textQuote', TextQuoteDirective.directiveFactory);
+angular.module('angularEs6DemoApp').directive('textQuote', $log => new TextQuoteDirective($log));
